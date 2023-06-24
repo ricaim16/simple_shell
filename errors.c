d8fd0a03829fb6431b58caa2c709379e99bf6876
GDesigns49 <120295473+GDesigns49@users.noreply.github.com>
2023-06-24 17:01:05 +0100

    errors.c

--- errors.c
+++ errors.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * print_error - prints error messages to standard error
+ * print_error - prints the error messages to standard error
  * @vars: pointer to struct of variables
- * @msg: message to print out
+ * @msg: the message to print out
  *
  * Return: void
  */
