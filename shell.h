650ab4f687dba9be5b159f2791f0e35a7bc9817a
GDesigns49 <120295473+GDesigns49@users.noreply.github.com>
2023-06-24 17:15:35 +0100

    shell.h

--- shell.h
+++ shell.h
@@ -1,16 +1,15 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <limits.h>
-#include <signal.h>
-
 
 /**
  * struct variables - variables
